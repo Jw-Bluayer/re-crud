d8725d19a3aa1641287b18f2d042823c2989c5c5
Jw-Bluayer <wjddn0728@naver.com>
2018-01-19 22:11:20 +0900

    re-crud final

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,38 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
+    @post = Post.new
+  end
+
+  def show
+    @post = Post.find(params[:id])
   end
 
   def create
+    #form_tag
+    # Post.create(title: params[:title], content: params[:content])
+
+    #form_for
+    Post.create(title: params[:post][:title], content: params[:post][:content])
+    redirect_to '/posts'
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    @post.update_attributes(title: params[:post][:title], content: params[:post][:content])
+    redirect_to '/posts'
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to '/posts'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
 
 get '/posts' => 'posts#index'
-post '/posts/create' => 'posts#create'
+post '/posts' => 'posts#create'
 
 get '/posts/new' => 'posts#new'
 
-get '/posts/:id/edit' => 'posts#edit'
+get '/posts/:id/edit' => 'posts#edit', as: "edit_post"
 
 put '/posts/:id' => 'posts#update'
 patch '/posts/:id' => 'posts#update'
 
 delete '/posts/:id' => 'posts#destroy'
 
-get '/posts/:id' => 'posts#show'
+get '/posts/:id' => 'posts#show', as: "post"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
